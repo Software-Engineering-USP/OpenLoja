b916fb5a79b62bbd8fc7675373d6d98758bdf096
Raffael Raiel Trindade <raffaelraiel@usp.br>
2026-07-07 16:58:55 -0300

    Reorganizes files

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
-
 # imports necessários para o funcionamento do projeto
-from fastapi import FastAPI, Request,Depends, HTTPException, status, Cookie, Response, Form
-from fastapi.responses import HTMLResponse, Response
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from typing import Annotated
-from sqlmodel import SQLModel, create_engine, Session, select
+from sqlmodel import SQLModel, create_engine
 
-# setup do Fastapi 
+# setup do Fastapi
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -17,16 +14,17 @@ arquivo_sqlite = "database.db"
 url_sqlite = f"sqlite:///{arquivo_sqlite}"
 engine = create_engine(url_sqlite)
 
+
 def create_db():
     SQLModel.metadata.create_all(engine)
-    
+
+
 @app.on_event("startup")
 def on_startup() -> None:
     create_db()
 
+
 # rota inicial para criação de contas
 @app.get("/")
-async def root(request : Request):
-    return templates.TemplateResponse(
-        request=request, name="createAccount.html"
-    )
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="createAccount.html")
--- src/models.py
+++ src/models.py
@@ -1,11 +1,13 @@
 from sqlmodel import SQLModel, Field, Relationship
 
+
 class Vendedor(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     nome: str
     senha: str
     estacao_em_uso_id: int | None = Field(default=None, foreign_key="estacao.id")
     reservas: list["Reserva"] = Relationship(back_populates="vendedor")
+
 
 class Cliente(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -14,12 +16,14 @@ class Cliente(SQLModel, table=True):
     reservas: list["Reserva"] = Relationship(back_populates="cliente")
     avaliacoes: list["Avaliacao"] = Relationship(back_populates="cliente")
 
+
 class Loja(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     nota: int
     nome: str
     vendedor_id: int | None = Field(default=None, foreign_key="vendedor.id")
     produtos: list["Produto"] = Relationship(back_populates="loja")
+
 
 class Produto(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -29,6 +33,7 @@ class Produto(SQLModel, table=True):
     loja_id: int | None = Field(default=None, foreign_key="loja.id")
     reserva_id: int | None = Field(default=None, foreign_key="reserva.id")
 
+
 class Reserva(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     produto_id: int | None = Field(default=None, foreign_key="produto.id")
@@ -37,6 +42,7 @@ class Reserva(SQLModel, table=True):
     vendedor: "Vendedor" = Relationship(back_populates="reservas")
     cliente: "Cliente" = Relationship(back_populates="reservas")
 
+
 class Avaliacao(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     nota: int
@@ -44,4 +50,3 @@ class Avaliacao(SQLModel, table=True):
     dia: int
     horario: int
     produto_id: int | None = Field(default=None, foreign_key="produto.id")
-
